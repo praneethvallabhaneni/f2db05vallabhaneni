e92514e4083d5bc15861a2eaa3c1df572adc727c
praneethvallabhaneni <71108379+praneethvallabhaneni@users.noreply.github.com>
2022-11-09 22:50:52 -0600

    updated files to git

--- controllers/engine.js
+++ controllers/engine.js
@@ -30,8 +30,25 @@ exports.engine_detail = function(req, res) {
 }; 
  
 // Handle Costume create on POST. 
-exports.engine_create_post = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Engine create POST'); 
+exports.engine_create_post = async function(req, res) { 
+    console.log(req.body) 
+    let document = new Costume(); 
+    // We are looking for a body, since POST does not have query parameters. 
+    // Even though bodies can be in many different formats, we will be picky 
+    // and require that it be a json object 
+    // {"costume_type":"goat", "cost":12, "size":"large"} 
+    document.bikeType = req.body.bikeType; 
+    document.Cylinders = req.body.Cylinders; 
+    document.FuelType = req.body.FuelType;
+    document.Transmission = req.body.Transmission; 
+    try{ 
+        let result = await document.save(); 
+        res.send(result); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
 }; 
  
 // Handle Costume delete form on DELETE. 
